3b0c5802138916b2df28edc86f48db76caf24ed2
Abderrahmane Mouzoune <abderrahmane_mouzoune@carrefour.com>
2024-05-07 12:19:43 +0200

    add credits and theme colors

---
+++ src/components/footer.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link"
+import React from "react"
+import { Button } from "@/components/ui/button"
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons"
+
+export default function Footer() {
+  return (
+    <footer className="bg-accent py-5 text-sm">
+      <section className="container flex justify-between">
+        <p>
+          © {new Date().getFullYear()}, Built by{" "}
+          <Button variant={"link"} className="px-0 pr-1" asChild>
+            <Link href={"https://abderrahmanemouzoune.com"}>
+              Abderrahmane Mouzoune.
+            </Link>
+          </Button>
+          and powered by{" "}
+          <Button variant={"link"} className="px-0" asChild>
+            <Link href={"https://www.youzoune.com"}>youzoune.com</Link>
+          </Button>
+        </p>
+
+        <div className="flex gap-2">
+          <Link
+            href={"https://github.com/AbderrahmaneMouzoune"}
+            className="hover:text-primary transition-colors"
+            target="_blank"
+          >
+            <GitHubLogoIcon className="size-6" />
+          </Link>
+
+          <hr className="size-6 w-[1px] bg-accent-foreground" />
+
+          <Link
+            href={"https://www.linkedin.com/in/abderrahmane-mouzoune/"}
+            className="hover:text-primary transition-colors"
+            target="_blank"
+          >
+            <LinkedInLogoIcon className="size-6" />
+          </Link>
+        </div>
+      </section>
+    </footer>
+  )
+}
