897402192f9a50cc6b5754d6a77abb764d46d6ba
AbderrahmaneMouzoune <abderrahmane.mouzoune@gmail.com>
2024-05-23 15:13:02 +0200

    add spacing between lang

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import LangToggle from './locale-switcher-select'
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-10 flex justify-end p-5">
+    <header className="fixed top-0 left-0 right-0 z-10 flex justify-end p-5 gap-2">
       <ThemeToggle />
       <LangToggle />
     </header>
